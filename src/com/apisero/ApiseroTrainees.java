6db7a503e1bd9acabd3ae88a4ebd88a2f172eddc
DELL <DELL@DESKTOP-JLP31BF>
2020-10-26 14:53:50 +0530

    hello mahesh this is kishan raj's first commit.

--- src/com/apisero/ApiseroTrainees.java
+++ src/com/apisero/ApiseroTrainees.java
@@ -5,6 +5,7 @@ public class ApiseroTrainees {
 	public static void main(String[] args) {
     System.out.println("hi this is mahesh karad");
     System.out.println("hello this is Lakhan Mane");
+    System.out.println("hello this is kishan raj");
 
 	}
 
